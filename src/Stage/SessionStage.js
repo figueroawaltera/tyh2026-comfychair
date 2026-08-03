bb09c69fc4d8146a86a5fec37a44217c98143814
Walter Figueroa <figueroa.walter.a@gmail.com>
2026-08-02 23:24:01 -0300

    Agregar reglas base de transición de estados

--- src/Stage/SessionStage.js
+++ src/Stage/SessionStage.js
@@ -1,43 +1,51 @@
-class SessionStage{
-    constructor(Session){
-        this._Session = Session;
+class SessionStage {
+    constructor(session) {
+        this._Session = session;
     }
 
-    closeStage(){
+    belongsTo(session) {
+        return this._Session === session;
     }
 
-    canSubmit(paper){
-        return false
+    canTransitionTo(nextStage) {
+        return false;
     }
 
-    submit(paper){
+    closeStage() {
+        throw new Error("No existe una etapa siguiente para el estado actual.");
+    }
+
+    canSubmit(paper) {
+        return false;
+    }
+
+    submit(paper) {
         throw new Error("Cannot submit papers at this stage");
     }
 
-    enterBid(paper, reviewer, interest){
+    enterBid(paper, reviewer, interest) {
         throw new Error("Cannot enter bids from the current stage.");
     }
 
-    enterAssigment(paper, reviewer){
-        throw new Error("Cannot assigment from the current stage.");        
-    }
-
-    asignarRevisores(){
+    enterAssigment(paper, reviewer) {
         throw new Error("Cannot assigment from the current stage.");
     }
 
-    enterReview(paper, reviewer, review, score){
+    asignarRevisores() {
+        throw new Error("Cannot assigment from the current stage.");
+    }
+
+    enterReview(paper, reviewer, review, score) {
         throw new Error("Cannot enter review from the current stage.");
     }
 
-    obtenerArticulosOrdenadosPorScore(){
-       throw new Error("Cannot return sort papers from the current stage.");
+    obtenerArticulosOrdenadosPorScore() {
+        throw new Error("Cannot return sort papers from the current stage.");
     }
 
-    obtenerArticulosAceptados(){
+    obtenerArticulosAceptados() {
         throw new Error("Cannot return accepted papers from the current stage.");
     }
-
 }
 
 module.exports = SessionStage;
