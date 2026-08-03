57b22a96ce5afbc2ebcfa39ad0d730fc3848feeb
Walter Figueroa <figueroa.walter.a@gmail.com>
2026-08-03 00:40:30 -0300

    Mover identidad y prioridad al Bid

--- src/Bid.js
+++ src/Bid.js
@@ -1,28 +1,50 @@
 const Interests = {
-	Interested: Symbol("Interested"),
-	Maybe: Symbol("Maybe"),
-	NotInterested: Symbol("NotInterested"),
-	Conflict: Symbol("Conflict")
+    Interested: Symbol("Interested"),
+    Maybe: Symbol("Maybe"),
+    NotInterested: Symbol("NotInterested"),
+    Conflict: Symbol("Conflict")
 };
 
-class Bid{
-    constructor(paper, reviewer, interest){
+class Bid {
+    constructor(paper, reviewer, interest) {
         this._paper = paper;
         this._reviewer = reviewer;
         this._interest = interest;
-
     }
-    paper(){
+
+    static priorityFor(interest) {
+        if (interest === Interests.Interested) return 2;
+        if (interest === Interests.Maybe) return 1;
+        return 0;
+    }
+
+    matches(paper, reviewer) {
+        return this._paper === paper && this._reviewer === reviewer;
+    }
+
+    priority() {
+        return Bid.priorityFor(this._interest);
+    }
+
+    hasConflict() {
+        return this._interest === Interests.Conflict;
+    }
+
+    paper() {
         return this._paper;
     }
-    reviewer(){
+
+    reviewer() {
         return this._reviewer;
     }
-    interest(){
+
+    interest() {
         return this._interest;
     }
-    setInterest(interest){
+
+    setInterest(interest) {
         this._interest = interest;
     }
 }
-module.exports = {Bid, Interests};
+
+module.exports = { Bid, Interests };
