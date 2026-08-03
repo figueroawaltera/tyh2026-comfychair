58b646957a20d85283c7810cc48dde132eb8e8c4
Walter Figueroa <figueroa.walter.a@gmail.com>
2026-08-02 22:57:40 -0300

    Encapsular transición desde revisión

--- src/Stage/RevisionStage.js
+++ src/Stage/RevisionStage.js
@@ -1,30 +1,21 @@
 const SessionStage = require("./SessionStage");
 const SelectionStage = require("./SelectionStage");
 
-class RevisionStage extends SessionStage{
-    constructor(Session){
-        super(Session);
+class RevisionStage extends SessionStage {
+    closeStage() {
+        this._Session._changeStage(new SelectionStage(this._Session));
     }
 
-    closeStage(){
-        let newStage = new SelectionStage(this._Session)
-        this._Session.changeStage(newStage)
-        return newStage
-    }
-
-    enterReview(paper, reviewer, review, score){
-        if(this._Session.assigmentExistsFor(paper, reviewer)){
-            if(paper.reviewExistsFor(reviewer)){
-                throw new Error("El reviewer ya ingreso una review para este paper.");
-            }
-            else{
-                paper.addReview(reviewer,review,score) 
-            }           
-        }
-        else{
+    enterReview(paper, reviewer, review, score) {
+        if (!this._Session.assigmentExistsFor(paper, reviewer)) {
             throw new Error("Reviewer no autorizado.");
         }
-        
+
+        if (paper.reviewExistsFor(reviewer)) {
+            throw new Error("El reviewer ya ingreso una review para este paper.");
+        }
+
+        paper.addReview(reviewer, review, score);
     }
 }
 
