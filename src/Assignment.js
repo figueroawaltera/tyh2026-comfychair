19e981490adba0f0fe757db5be010b01cecd80eb
Walter Figueroa <figueroa.walter.a@gmail.com>
2026-08-03 00:40:19 -0300

    Enriquecer Assignment con comportamiento de dominio

--- src/Assignment.js
+++ src/Assignment.js
@@ -4,6 +4,20 @@ class Assignment {
         this._reviewer = reviewer;
     }
 
+    static create(paper, reviewer) {
+        paper.assignReviewer(reviewer);
+        reviewer.assignPaper();
+        return new Assignment(paper, reviewer);
+    }
+
+    matches(paper, reviewer) {
+        return this._paper === paper && this._reviewer === reviewer;
+    }
+
+    isForPaper(paper) {
+        return this._paper === paper;
+    }
+
     paper() {
         return this._paper;
     }
