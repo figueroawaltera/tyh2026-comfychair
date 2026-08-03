e82f3a9b026af49b4674be05c867f1d36fa03294
Walter Figueroa <figueroa.walter.a@gmail.com>
2026-08-02 22:57:13 -0300

    Encapsular transición desde recepción

--- src/Stage/ReceivingStage.js
+++ src/Stage/ReceivingStage.js
@@ -1,24 +1,18 @@
 const SessionStage = require("./SessionStage");
 const BiddingStage = require("./BiddingStage");
 
-class ReceivingStage extends SessionStage{
-    constructor(Session){
-        super(Session);
-    }
-
-    canSubmit(paper){
+class ReceivingStage extends SessionStage {
+    canSubmit(paper) {
         return paper.isValid();
     }
 
-    submit(paper){        
+    submit(paper) {
         if (!this.canSubmit(paper)) throw new Error("Cannot submit invalid paper");
         this._Session.papers().push(paper);
     }
 
-    closeStage(){
-        let newStage = new BiddingStage(this._Session)
-        this._Session.changeStage(newStage)
-        return newStage
+    closeStage() {
+        this._Session._changeStage(new BiddingStage(this._Session));
     }
 }
 
