e44f7525c0afb3b1d7ef7205218130723d23f105
Walter Figueroa <figueroa.walter.a@gmail.com>
2026-08-02 22:57:21 -0300

    Encapsular transición desde bidding

--- src/Stage/BiddingStage.js
+++ src/Stage/BiddingStage.js
@@ -1,28 +1,19 @@
 const SessionStage = require("./SessionStage");
 const AssigmentStage = require("./AssigmentStage");
-const {Bid, Interests} = require("../Bid");
+const { Bid } = require("../Bid");
 
-class BiddingStage extends SessionStage{
-    constructor(Session){
-        super(Session);
+class BiddingStage extends SessionStage {
+    closeStage() {
+        this._Session._changeStage(new AssigmentStage(this._Session));
     }
 
-    closeStage(){
-        let newStage = new AssigmentStage(this._Session)
-        this._Session.changeStage(newStage)
-        return newStage
-    }
-
-    enterBid(paper, reviewer, interest){
-        if(this._Session.bidExistsFor(paper, reviewer)){
-            let existing =  this._Session.bidFor(paper, reviewer);
+    enterBid(paper, reviewer, interest) {
+        if (this._Session.bidExistsFor(paper, reviewer)) {
+            const existing = this._Session.bidFor(paper, reviewer);
             existing.setInterest(interest);
+        } else {
+            this._Session.bids().push(new Bid(paper, reviewer, interest));
         }
-        else{
-            let bid = new Bid(paper, reviewer, interest);
-            this._Session.bids().push(bid);
-        }
-        
     }
 }
 
